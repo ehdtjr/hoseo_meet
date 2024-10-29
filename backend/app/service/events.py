0a26676989ca200ff3bf20385b2b81f88d6ec0b8
11kkw <11kkw@naver.com>
2024-10-29 21:32:25 +0900

    FIX: project_id 불일치로 인한 메시지 전송 오류 수정
    - project_id 기존 hoseo-meet -> hoseo-meet-7918f 로 수정하였습니다
    
    close #7

--- backend/app/service/events.py
+++ backend/app/service/events.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 fcm = FCMNotification(service_account_file="hoseo-meet-firebase-adminsdk"
-"-oswm2-1186906f2e.json", project_id="hoseo-meet")
+"-oswm2-1186906f2e.json", project_id="hoseo-meet-7918f")
 
 class SendEventProtocol:
     async def send(self, event_data: dict):
