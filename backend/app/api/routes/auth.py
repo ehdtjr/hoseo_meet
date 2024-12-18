0950684a44fe0324218626ef4eec82550b13ffbe
dongseok <dskang207@gmail.com>
2024-12-18 19:36:46 +0900

    add jwt

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -1,28 +1,28 @@
+from app.service.auth import CustomJWTStrategy
 from fastapi import APIRouter, Depends, HTTPException, status, Header
-
-from app.core.security import auth_backend
+from app.core.security import auth_backend, get_custom_jwt_strategy
 from app.service.email import EmailVerificationService, get_email_verification_service
 from app.schemas.user import UserRead, UserCreate, UserUpdate
 from app.core.security import fastapi_users
 from app.service.user import UserManager, get_user_manager
 from typing import Optional
 from app.schemas.user import UserUpdate
-from app.models.user import User, OAuthAccount  # User 모델 import 필요
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import APIKeyHeader, OAuth2PasswordRequestForm
 from app.schemas.auth import Token
-from app.service.auth import generate_tokens, verify_token
 from fastapi_users.manager import BaseUserManager
 from fastapi_users import models
 from fastapi_users.router.common import ErrorCode
 from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi_users.jwt import decode_jwt
+from app.core.config import settings
+import jwt
 
 router = APIRouter()
 
 api_key_header = APIKeyHeader(name="Authorization", auto_error=False)
 
+# 이메일 인증
 @router.get("/verify-email", tags=["auth"])
 async def verify_email(
     token: str,
@@ -43,16 +43,12 @@ async def verify_email(
     await user_manager.activate_user(user_data.id)
     return {"message": "Email verified successfully", "user": user_data}
 
-
-# 인증 및 사용자 관련 라우터 추가
-router.include_router(
-    fastapi_users.get_auth_router(auth_backend), prefix="/jwt", tags=["auth"]
-)
-
-@router.post("/login", response_model=Token)
+# 로그인
+@router.post("/login", tags=["auth"])
 async def login(
     credentials: OAuth2PasswordRequestForm = Depends(),
     user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+    jwt_strategy: CustomJWTStrategy = Depends(get_custom_jwt_strategy),
     ):
     
     user = await user_manager.authenticate(credentials)
@@ -63,24 +59,57 @@ async def login(
                 detail=ErrorCode.LOGIN_BAD_CREDENTIALS,
             )
 
-    tokens = await generate_tokens(user)
-    return tokens
+    #access_token, refresh_token 생성
+    tokens = await jwt_strategy.write_token(user)
 
-@router.post("/refresh", response_model=Token)
-async def refresh_token(
-    authorization: str = Depends(api_key_header),
+    return {"message": "login successful", "access_token": tokens["access_token"], "refresh_token": tokens["refresh_token"]}   
+
+# 토큰 재발급
+# @router.post("/refresh", response_model=Token)
+# async def refresh_token(
+#     authorization: str = Depends(api_key_header),
+# ):
+#     if not authorization or not authorization.startswith("Bearer "):
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Invalid or missing refresh token",
+#         )
+#     refresh_token = authorization.split(" ")[1]
+#     user_id = await verify_token(refresh_token, token_type="refresh")
+#     # 사용자 유효성 검사
+#     tokens = await generate_tokens(user_id)
+#     return tokens
+
+# JWT 토큰 디코딩
+@router.get("/jwt/decode", tags=["auth"])
+async def decode_jwt_token(
+    authorization: Optional[str] = Depends(api_key_header),  # Authorization 헤더에서 JWT 토큰 추출
 ):
-    if not authorization or not authorization.startswith("Bearer "):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or missing refresh token",
+    
+    token = authorization.split(" ")[1]
+    try:
+        # JWT 디코딩
+        payload = decode_jwt(
+            token,
+            secret=settings.SECRET_KEY,
+            audience=["fastapi-users:auth"],
+            algorithms=["HS256"],
         )
-    refresh_token = authorization.split(" ")[1]
-    user_id = await verify_token(refresh_token, token_type="refresh")
-    # 사용자 유효성 검사
-    tokens = await generate_tokens(user_id)
-    return tokens
+        return {"message": "Token decoded successfully", "payload": payload}
 
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired"
+        )
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
+
+# 인증 및 사용자 관련 라우터 추가
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/jwt", tags=["auth"]
+)
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate), tags=["auth"]
 )
--- backend/app/service/auth.py
+++ backend/app/service/auth.py
@@ -1,22 +1,71 @@
-from app.core.security import create_access_token, create_refresh_token, decode_token
-from app.models.user import User
+from datetime import datetime, timedelta
+from fastapi_users.authentication.strategy import JWTStrategy
+from fastapi_users import models
+import jwt
+from app.core.config import settings
+from datetime import datetime, timedelta, timezone
+from fastapi import HTTPException
+import httpx
 
-async def generate_tokens(user: User) -> dict:
-    """
-    Generate access_token and refresh_token for the given user.
-    """
-    
-    return {
-        "access_token": create_access_token(user.id),
-        "refresh_token": create_refresh_token(user.id),
-    }
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = "HS256"
 
-async def verify_token(token: str, token_type: str) -> str:
-    """
-    Verify the given token (access or refresh).
-    """
+
+class CustomJWTStrategy(JWTStrategy):
+    def __init__(self, secret: str, lifetime_seconds: int):
+        super().__init__(secret, lifetime_seconds)
+        self.access_lifetime_seconds = lifetime_seconds
+        self.refresh_lifetime_seconds = lifetime_seconds * 24 * 14
+
+    def _generate_token(self, user_id: str, expires_in: int, token_type: str) -> str:
+        payload = {
+            "sub": f"{user_id}.{token_type}",
+            "exp": datetime.now(timezone.utc) + timedelta(seconds=expires_in),
+            "aud": ["fastapi-users:auth"],
+        }
+        return jwt.encode(payload, self.secret, algorithm=ALGORITHM)
+
+    async def write_token(self, user: models.UP) -> dict:
+        access_token = self._generate_token(
+            user_id=str(user.id),
+            expires_in=self.lifetime_seconds,
+            token_type="access"
+        )
+        refresh_token = self._generate_token(
+            user_id=str(user.id),
+            expires_in=self.refresh_lifetime_seconds,
+            token_type="refresh"
+        )
+        return {"access_token": access_token, "refresh_token": refresh_token}
+
+    async def read_refresh_token(self, token: str) -> str:
+        try:
+            payload = jwt.decode(token, self.secret, algorithms=[ALGORITHM])
+            return payload.get("sub")
+        except jwt.ExpiredSignatureError:
+            raise ValueError("Refresh token expired")
+        except jwt.InvalidTokenError:
+            raise ValueError("Invalid refresh token")
+
+    async def decode_token(token: str) -> dict:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+
+# 카카오 user 정보 조회
+async def get_kakao_user_data(access_token: str) -> dict:
+    headers = {"Authorization": f"Bearer {access_token}"}
     try:
-        payload = decode_token(token)
-        return payload.get("sub")
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                "https://kapi.kakao.com/v2/user/me", headers=headers
+            )
+            response.raise_for_status()  # 요청 오류 시 예외 발생
+            kakao_user_data = response.json()
+            return kakao_user_data
+
+    except httpx.RequestError as e:
+        raise HTTPException(status_code=400, detail=f"Request Error: {e}")
     except Exception as e:
-        raise ValueError(f"Invalid {token_type} token: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"An unexpected error occurred: {e}"
+        )
