ee5eb1ab19b769c806bc7ec9cc1325471b139d5d
KIMGEONU <11kkw17@gmail.com>
2025-01-02 06:24:28 +0900

    pubspec

--- frontend/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import firebase_core
 import firebase_messaging
 import flutter_local_notifications
 import flutter_secure_storage_macos
+import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
 import webview_flutter_wkwebview
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
