300680226091dacb88d223da55cf7aa4f6e959b7
11kkw <11kkw17@gmail.com>
2025-01-14 10:45:06 +0900

    Feat: MeetPostService에 is_subscribed 정보 반환 로직 추가
    - get_filtered_meet_posts 및 get_detail_meet_post 메서드에 사용자의 구독 여부(is_subscribed) 정보를 포함하도록 수정
    - 사용자가 해당 게시글에 구독한 경우 is_subscribed 값을 True로 설정하여 응답 반환
    - MeetPostListResponse 및 MeetPostResponse에 구독 여부 필드 추가

--- backend/app/api/routes/meet_post.py
+++ backend/app/api/routes/meet_post.py
@@ -38,19 +38,21 @@ async def get_filtered_meet_posts(
     content: Optional[str] = None,
     skip: int = 0,
     limit: int = 10,
+    user: User = Depends(current_active_user),
     db: AsyncSession = Depends(get_async_session),
     meet_post_service: MeetPostServiceProtocol = Depends(get_meet_post_service),
 ):
     result = await meet_post_service.get_filtered_meet_posts(
-        db, title, type, content, skip, limit)
+        db, user, title, type, content, skip, limit)
     return result
+
 
 @router.post("/subscribe/{meet_post_id}")
 async def subscribe_to_meet_post(
-        meet_post_id: int,
-        db: AsyncSession = Depends(get_async_session),
-        user: User = Depends(current_active_user),
-        meet_post_service: MeetPostServiceProtocol = Depends(get_meet_post_service),
+    meet_post_id: int,
+    db: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_active_user),
+    meet_post_service: MeetPostServiceProtocol = Depends(get_meet_post_service),
 ):
     """
     특정 meet_post에 대해 사용자를 구독시키는 엔드포인트입니다.
@@ -68,10 +70,12 @@ async def subscribe_to_meet_post(
         # 적절한 예외 메시지와 함께 400 에러 반환
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
+
 @router.get("/detail/{meet_post_id}", response_model=MeetPostResponse)
 async def get_meet_post_detail(
     meet_post_id: int,
     request: Request,
+    user: User = Depends(current_active_user),
     db: AsyncSession = Depends(get_async_session),
     meet_post_service: MeetPostServiceProtocol = Depends(get_meet_post_service),
 ):
@@ -81,7 +85,17 @@ async def get_meet_post_detail(
     - IP 기반으로 24시간 이내 중복 조회수 증가를 방지합니다.
     """
     client_ip = request.client.host
-    detail = await meet_post_service.get_detail_meet_post(db, meet_post_id, client_ip)
+    detail = await meet_post_service.get_detail_meet_post(
+        db=db,
+        user=user,
+        meet_post_id=meet_post_id,
+        ip_address=client_ip
+    )
+
     if detail is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Meet post not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Meet post not found"
+    )
+
     return detail
