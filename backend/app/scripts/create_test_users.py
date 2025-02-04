e53ef1141f3981f7047399c98e7aa54d56e6f326
11kkw <11kkw@naver.com>
2025-02-04 15:53:13 +0900

    Fix(test-user):테스트 계정 is_verified True 처리 및 이메일 인증 우회

--- backend/app/scripts/create_test_users.py
+++ backend/app/scripts/create_test_users.py
@@ -1,7 +1,12 @@
 import asyncio
+from typing import Optional
+
 from faker import Faker
+from fastapi_users.models import UP
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from starlette.requests import Request
+
 from app.schemas.user import UserCreate
 from app.models.user import User as UserModel
 from app.service.user import UserManager
@@ -20,19 +25,31 @@ expire_on_commit=False)
 class TestUserManager(UserManager):
     async def create(self, user_create, safe=True, request=None):
         # email_service가 None일 때 이메일 검증을 생략
-        if self.email_service is not None and not self.email_service.validate_email_domain(
-                user_create.email):
+        if self.email_service is not None and not self.email_service.validate_email_domain(user_create.email):
             raise ValueError("Invalid email domain.")
-        return await super().create(user_create, safe=safe, request=request)
+        # user_create의 is_verified 플래그를 True로 지정
+        user_create.is_verified = True
+        # 사용자 생성 후
+        created_user = await super().create(user_create, safe=safe, request=request)
+        # 생성된 사용자가 is_verified가 False라면 강제로 업데이트.
+        if not created_user.is_verified:
+            update_data = {"is_verified": True}
+            created_user = await self.user_db.update(created_user, update_data)
+        return created_user
+
+    # 이메일 인증 후 로직을 건너뛰기 위해 on_after_register를 오버라이드
+    async def on_after_register(self, user: UP, request: Optional[Request] = None) -> None:
+        pass
 
 
-async def create_test_users(user_manager, count=500):
+
+async def create_test_users(user_manager, count=10):
     for i in range(count):
         user_create = UserCreate(
-            email=f"test{i}@example.com",
-            password="testpassword123",  # 테스트 비밀번호
+            email=f"test{i}@vision.hoseo.edu",
+            password="test123",  # 테스트 비밀번호
             is_verified=True,  # 테스트 계정은 활성화된 상태로 설정
-            name=f"TestUser{i}",  # 추가된 필드
+            name=f"TaUser{i}",  # 추가된 필드
             gender="unknown"  # 추가된 필드
         )
         await user_manager.create(user_create, safe=True)
