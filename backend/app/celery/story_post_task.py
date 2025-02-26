fe5431a48ab5ffdee1f0e79e003a4633dcb8fbf7
11kkw <11kkw@naver.com>
2025-02-27 04:35:25 +0900

    Fix(story) 스토리 이미지 db 오류 해결

--- backend/app/celery/story_post_task.py
+++ backend/app/celery/story_post_task.py
@@ -1,25 +1,48 @@
+import logging
 from datetime import datetime, timezone
 
-from asgiref.sync import async_to_sync
-from sqlalchemy import delete
-
-from app.celery.worker import app
-from app.core.db import async_session_maker
+from pydantic_core import MultiHostUrl
+from sqlalchemy import create_engine, delete
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
 from app.models import StoryPost
+from app.celery.worker import app
 
 
-async def _delete_expired_story_posts():
-    try:
-        async with async_session_maker() as session:
-            now = datetime.now(timezone.utc)
-            print("now: {}".format(now))
-            stmt = delete(StoryPost).where(StoryPost.expires_at <= now)
-            await session.execute(stmt)
-            await session.commit()
-    except Exception as e:
-        print(f"Error deleting expired story posts: {e}")
-        await session.rollback()
+def SQLALCHEMY_DATABASE_URI() -> MultiHostUrl:
+    return MultiHostUrl.build(
+        scheme="postgresql",
+        username=settings.POSTGRES_USER,
+        password=settings.POSTGRES_PASSWORD,
+        host=settings.POSTGRES_SERVER,
+        port=settings.POSTGRES_PORT,
+        path=settings.POSTGRES_DB,
+    )
+
+
+sync_engine = create_engine(
+    str(SQLALCHEMY_DATABASE_URI()),
+    pool_size=20,
+    max_overflow=10,
+)
+
+# 동기 세션 메이커 생성
+sync_session_maker = sessionmaker(
+    sync_engine,
+    expire_on_commit=False,
+)
 
 @app.task
 def delete_expired_story_posts_task():
-    async_to_sync(_delete_expired_story_posts)()
+    with sync_session_maker() as session:
+        try:
+            now = datetime.now(timezone.utc)
+            logger = logging.getLogger(__name__)
+            logger.info(f"Deleting expired story posts at: {now}")
+            stmt = delete(StoryPost).where(StoryPost.expires_at <= now)
+            session.execute(stmt)
+            session.commit()
+        except Exception as e:
+            logger.error(f"Error deleting expired story posts: {e}")
+            session.rollback()
+            raise
