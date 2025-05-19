923385191d0fcef717384c6c95a003e953365efc
11kkw <11kkw@naver.com>
2025-05-20 01:14:43 +0900

    templates

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -4,7 +4,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
 from app.models.user import User
+from fastapi.templating import Jinja2Templates
 
+
+templates = Jinja2Templates(directory="app/templates")
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, User)
